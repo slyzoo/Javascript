0e65e6f1c5d47e867bab819fb4d14e9bb2a7c38e
slyzoo <lilithhimes@gmail.com>
2025-04-01 11:16:45 -0500

    lession 5

--- 5 variables/main.js
+++ 5 variables/main.js
@@ -1,20 +1,39 @@
 let x; // declaration
 x = 15; // assignment
 
+console.log(x);
+
 let y = 14; // both
 
+// int
 let age = 18;
+
+console.log(age);
+console.log(typeof age);
+console.log(`you are ${age} years old`);
+document.getElementById("age").textContent = ` your age is ${age}`;
+
+// float
 let price = 12.99;
 
+console.log(price);
+console.log(typeof price);
+console.log(`that can of soup is $${price}`);
+document.getElementById("price").textContent = `that can of soup is $${price}`;
+
+// string
 let firstname = "sly";
 let lastname = "zoo";
 
-console.log(x);
-console.log(age);
-console.log(`you are ${age} years old`);
 console.log(firstname);
+console.log(typeof firstname);
 console.log(`you are ${firstname}${lastname}`);
-console.log(price);
-console.log(`that can of soup is $${price}`);
+document.getElementById("name").textContent = `you are ${firstname}${lastname}`;
 
-console.log(typeof price);
+// boolean
+let online = true;
+
+console.log(online);
+console.log(typeof online);
+console.log(`you are online? : ${online}`);
+document.getElementById("online").textContent = `you are online? : ${online}`;
